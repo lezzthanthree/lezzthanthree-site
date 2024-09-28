213cb98a0b8b2295e4a23dead98193314703e602
Smilie Pop <arisatominato48@gmail.com>
2024-09-28 21:58:22 +0800

    Made navbar component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,63 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Navigation.module.scss";
+import Logo from "@/public/img/sp_dedede.png";
+import { usePathname } from "next/navigation";
+
 const Navigation = () => {
-    return <h1> Hello world </h1>;
+    const route = usePathname();
+
+    return (
+        <header className={styles.dashboard__header}>
+            <Link href="/" className={styles.dashboard__home}>
+                <Image src={Logo} alt="Logo" width={60} height={60} />
+            </Link>
+            <nav className={styles.dashboard__nav}>
+                <Link
+                    href="/blogs"
+                    className={
+                        route == "/blogs"
+                            ? styles.dashboard__nav_link_selected
+                            : styles.dashboard__nav_link
+                    }
+                >
+                    blogs.
+                </Link>
+                <Link
+                    href="/codes"
+                    className={
+                        route == "/codes"
+                            ? styles.dashboard__nav_link_selected
+                            : styles.dashboard__nav_link
+                    }
+                >
+                    codes.
+                </Link>
+                <Link
+                    href="/edits"
+                    className={
+                        route == "/edits"
+                            ? styles.dashboard__nav_link_selected
+                            : styles.dashboard__nav_link
+                    }
+                >
+                    edits.
+                </Link>
+                <Link
+                    href="/contacts"
+                    className={
+                        route == "/contacts"
+                            ? styles.dashboard__nav_link_selected
+                            : styles.dashboard__nav_link
+                    }
+                >
+                    contacts.
+                </Link>
+            </nav>
+        </header>
+    );
 };
 
 export default Navigation;
