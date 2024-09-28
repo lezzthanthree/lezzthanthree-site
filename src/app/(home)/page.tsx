9f0529eabfa3cab06dcc9269da20feb34bc3e056
Smilie Pop <arisatominato48@gmail.com>
2024-09-28 21:57:35 +0800

    Added (home) folder

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,12 @@
 import styles from "./page.module.scss";
 import Link from "next/link";
 import Image from "next/image";
+import Logo from "@/public/img/sp_dedede.png";
 
 export default function Home() {
     return (
         <main className={styles.home}>
-            <Image
-                src="/img/sp_dedede.png"
-                alt="Logo"
-                width={256}
-                height={256}
-            />
+            <Image src={Logo} alt="Logo" width={256} height={256} />
             <nav className={styles.home_nav}>
                 <Link className={styles.home_nav_links} href="/blogs">
                     blogs.
